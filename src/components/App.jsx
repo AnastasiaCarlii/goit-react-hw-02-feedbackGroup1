ba37c8f1250cb1ac0f78bd6fa2bf18158fe69b90
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-08-08 22:57:36 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGoodFeedback = () => {
+    this.setState(prevState => ({ good: prevState.good + 1 }));
+  };
+
+  handleNeutralFeedback = () => {
+    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+  };
+
+  handleBadFeedback = () => {
+    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleGoodFeedback}>Good</button>
+        <button onClick={this.handleNeutralFeedback}>Neutral</button>
+        <button onClick={this.handleBadFeedback}>Bad</button>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// return (
+//   <div
+//     style={{
+//       height: '100vh',
+//       display: 'flex',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       fontSize: 40,
+//       color: '#010101'
+//     }}
+//   >
+//   <
+//   </div>);
